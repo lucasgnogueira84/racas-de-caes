b0014281b1e7167cca010ba88db757ea39d72094
lucasgnogueira84 <luctj84@gmail.com>
2024-09-08 10:02:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+function verificarEnter(event) {
+    if (event.keyCode === 13) { // 13 é o código da tecla Enter
+      pesquisar(); // Chama sua função de pesquisa
+    }
+  }
+
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar a raça ou uma caracteristica</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let tags = "";
+    
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+            <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-resultado">
+            <div> 
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+        titulo: "Buldogue Francês",
+        descricao: "Com suas orelhas de morcego e focinho achatado, o Buldogue Francês é um cão charmoso e brincalhão. São companheiros leais e se adaptam bem à vida em apartamento.",
+        link: "https://pt.wikipedia.org/wiki/Buldogue_Francês",
+        imagem: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSuzkpZTxl_k1pdErnX9R_bPxxeC3YVE8QOrvofcOUsZYJMUxTW",
+        tags: "buldogue frances cachorro cão pet"
+    },
+    {
+        titulo: "Golden Retriever",
+        descricao: "Conhecido por sua natureza gentil e inteligência, o Golden Retriever é um cão de família popular. São ativos, adoram brincar e são ótimos com crianças.",
+        link: "https://pt.wikipedia.org/wiki/Golden_Retriever",
+        imagem: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRTfat1MSqyUVRBA063tHSr3AZmTqL5T3Kvd2VOzDLWYB7sVznx",
+        tags: "golden retriever cachorro cão pet familia"
+    },
+    {
+        titulo: "Pastor Alemão",
+        descricao: "Famosos por sua inteligência e lealdade, os Pastores Alemães são cães versáteis, frequentemente usados como cães de trabalho. São protetores e aprendem rápido.",
+        link: "https://pt.wikipedia.org/wiki/Pastor-alemão",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4ir7qxd2_RlqMp6l12zjeLI_EE3sO1ulMZYDrHrlN-x2SbHu",
+        tags: "pastor alemao cachorro cão pet trabalho"
+    },
+    {
+        titulo: "Poodle",
+        descricao: "Disponível em três tamanhos, o Poodle é conhecido por sua pelagem encaracolada e inteligência. São cães ativos que adoram aprender truques.",
+        link: "https://pt.wikipedia.org/wiki/Poodle",
+        imagem: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRicN2u_HIcESUS8Mhrr-KS9E7HfjisIOZQDcnCpOnB1htUF4Cw",
+        tags: "poodle cachorro cão pet inteligente"
+    },
+    {
+        titulo: "Yorkshire Terrier",
+        descricao: "Apesar do tamanho pequeno, o Yorkshire Terrier tem uma grande personalidade. São cães leais, cheios de energia e adoram ser o centro das atenções.",
+        link: "https://pt.wikipedia.org/wiki/Yorkshire_Terrier",
+        imagem: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQC5qNvtQUdFvFdOVhurco8HcIQZzM7VzZ6aJZ9JoysBIlkcGeZ",
+        tags: "yorkshire terrier cachorro cão pet pequeno"
+    }
+];
